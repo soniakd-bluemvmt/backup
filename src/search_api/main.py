66d18e2be043e68ea30cc95886987c181b0ab778
Sonia Daneshwar <sonia.daneshwar@bluemvmt.comr>
2025-07-09 15:08:15 -0400

    build

--- src/search_api/main.py
+++ src/search_api/main.py
@@ -2,14 +2,12 @@ from fastapi import FastAPI
 from .models.resource import Base
 from .db import engine
 from .routers import resource
-
-#Base.metadata.create_all(bind=engine)  # auto-create tables
+from .create_tables import create_tables
 
 app = FastAPI()
 
 @app.on_event("startup")
-def on_startup():
-    create_tables()
+async def on_startup():
+    await create_tables()
 
-    
 app.include_router(resource.router)  # register /v1/resource endpoints
