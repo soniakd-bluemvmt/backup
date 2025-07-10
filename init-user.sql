8cdf94eaaec0095aba8e419b7d000ee435f12911
Sonia Daneshwar <sonia.daneshwar@bluemvmt.comr>
2025-07-10 11:38:22 -0400

    .

--- init-user.sql
+++
@@ -1,5 +0,0 @@
--- Create user bluemvmt with password
-CREATE USER bluemvmt WITH PASSWORD 'thisispostgres';
-
--- Grant all privileges on database search_db to bluemvmt
-GRANT ALL PRIVILEGES ON DATABASE search_db TO bluemvmt;
